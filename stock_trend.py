7b62d22b47c5257cd2b44999c379375579aa8428
debaditya <debaditya@debadityas-MacBook-Air.local>
2024-06-30 19:30:31 +0530

    result missing

--- stock_trend.py
+++ stock_trend.py
@@ -351,7 +351,7 @@ if __name__ == "__main__":
     stock_symbol = input("Enter the stock symbol (e.g., TITAGARH.NS): ")
 
     period = "1y" # "1d", "1mo", "3mo", "6mo", "1y", "5y"
-    interval = "5d" # [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+    interval = "1d" # [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
     stock_data = get_stock_data(stock_symbol.upper(), period=period, interval=interval)
 
     get_stock_trend(stock_data=stock_data)
